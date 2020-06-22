e8364971943c95faa08d0043a41d9683013db206
skelsec <info@skelsec.com>
2020-06-22 16:10:01 -0700

    changing paradigm

--- multiplexor/server/transports/http11.py
+++ multiplexor/server/transports/http11.py
@@ -7,6 +7,7 @@ from multiplexor.server.packetizer import Packetizer
 from multiplexor.server.agent import MultiplexorAgent
 from multiplexor.logger.logger import mpexception, Logger
 
+
 class HTTP11Transport:
 	def __init__(self,reader, writer):
 		self.send_q = None
@@ -15,7 +16,7 @@ class HTTP11Transport:
 		self.reader = reader
 		self.writer = writer
 		self.incoming_task = None
-		self.template = b"""HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: %s\r\nDate: Mon, 18 Jul 2016 16:06:00 GMT\r\nEtag: "c561c68d0ba92bbeb8b0f612a9199f722e3a621a"\r\nKeep-Alive: timeout=60, max=997\r\nServer: Apache\r\n\r\n%s"""
+		self.template = b"""HTTP/1.1 200 OK\r\nConnection: Close\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: %s\r\nDate: Mon, 18 Jul 2016 16:06:00 GMT\r\nEtag: "c561c68d0ba92bbeb8b0f612a9199f722e3a621a"\r\nKeep-Alive: timeout=60, max=997\r\nServer: Apache\r\n\r\n%s"""
 
 	async def send(self, data):
 		#print('send %s' % data)
@@ -82,7 +83,7 @@ class HTTP11Transport:
 							remaining_bytes =  data_length - len(data)
 						else:
 							remaining_bytes = 0
-				#print('DD %s' % data)
+				print('DD %s' % data)
 				if len(data) > 5:
 
 					data = base64.b64decode(urllib.parse.unquote(data[5:].decode()).encode())
@@ -96,7 +97,7 @@ class HTTP11Transport:
 					response = self.template % (b'0', b'')
 				else:
 					senddata = base64.b64encode(senddata)
-					#print('senddata %s ' % senddata )
+					print('senddata %s ' % senddata )
 					response = self.template % (str(len(senddata)).encode(), senddata)
 				#print(response)
 				self.writer.write(response)
@@ -115,7 +116,7 @@ class HTTP11Transport:
 	async def run(self):
 		self.send_q = asyncio.Queue()
 		self.recv_q = asyncio.Queue()
-		self.incoming_task = asyncio.create_task(self.__incoming_handle())
+		#self.incoming_task = asyncio.create_task(self.__incoming_handle())
 
 
 class HTTP11TransportServer:
@@ -126,6 +127,7 @@ class HTTP11TransportServer:
 		self.logger = Logger('HTTP11TransportServer', logQ = logging_queue)
 		self.agent_dispatch_queue = None #will be defined by the server!
 		self.server = None
+		self.agent_temp = {} #agent's own made-up ids -> transport
 
 	@mpexception
 	async def terminate(self):
@@ -133,6 +135,93 @@ class HTTP11TransportServer:
 			self.server.close()		
 		if self.logger is not None:
 			await self.logger.terminate()
+
+	async def process_request(self, reader, writer):
+		try:
+			ibuff = b''
+			remaining_bytes = 4096
+			data = b''
+			headers_lower = {}
+			headers = {}
+
+			while remaining_bytes > 0:
+				if len(ibuff) > 1024*1024*10:
+					raise Exception('Incoming data too large!')
+				temp = await reader.read(remaining_bytes)
+				if temp == b'':
+					return
+				ibuff += temp
+				#print(ibuff)
+				if ibuff.find(b'\r\n\r\n') != 0:
+					hdr, data = ibuff.split(b'\r\n\r\n', 1)
+					headers = {}
+					
+					t_headers = hdr.split(b'\r\n')
+					if t_headers[0].startswith(b'POST') is False:
+						raise Exception('Not post request!')
+					for h in t_headers[1:]:
+						#print(h)
+						key, value = h.split(b': ',1)
+						key = key.decode('ascii')
+						headers[key] = value
+						headers_lower[key.lower()] = value
+					#print(headers)
+
+					if 'content-length' in headers_lower:
+						data_length = int(headers_lower['content-length'])
+						#print('data %s' % data)
+						#print('lendata %s' % len(data))
+						#print(data_length)
+						if len(data) >= data_length:
+							break
+						remaining_bytes =  data_length - len(data)
+					else:
+						remaining_bytes = 0
+			
+			#print('data %s' % data)
+			if 'agentid' not in headers_lower:
+				return
+			
+			if headers_lower['agentid'] not in self.agent_temp:
+				packetizer = Packetizer(self.logger.logQ) # packetizer must be created here, because it's parameters will change based on the transport!
+				transport = HTTP11Transport(reader, writer)
+				await transport.run()
+				self.agent_temp[headers_lower['agentid']] = transport
+				agent = MultiplexorAgent(self.logger.logQ, transport, packetizer)
+				await agent.run()
+				await self.agent_dispatch_queue.put((agent, 'CONNECTED'))
+				#await agent.wait_closed()
+				#await self.logger.info('Agent disconnected! %s:%s' % (remote_ip, remote_port))
+				#await self.agent_dispatch_queue.put((agent, 'DISCONNECTED'))
+
+			transport = self.agent_temp[headers_lower['agentid']]
+			#print('DD %s' % data)
+			if len(data) > 5:
+
+				data = base64.b64decode(urllib.parse.unquote(data[5:].decode()).encode())
+				#print(data)
+				await transport.recv_q.put(data)
+
+			try:
+				senddata = await asyncio.wait_for(transport.send_q.get(), 0.1)
+			except asyncio.TimeoutError:
+				#print('senddata %s ' % senddata )
+				response = transport.template % (b'0', b'')
+			else:
+				senddata = base64.b64encode(senddata)
+				#print('senddata %s ' % senddata )
+				response = transport.template % (str(len(senddata)).encode(), senddata)
+			#print(response)
+			writer.write(response)
+			await writer.drain()
+			writer.close()
+
+		except Exception as e:
+			print('__incoming_handle %s' % e)
+			import traceback
+			traceback.print_exc()
+		finally:
+			writer.close()
 	
 	@mpexception
 	async def handle_agent(self, reader, writer):
@@ -142,15 +231,7 @@ class HTTP11TransportServer:
 		"""
 		remote_ip, remote_port = writer.get_extra_info('peername')
 		await self.logger.info('Agent connected from %s:%s' % (remote_ip, remote_port))
-		packetizer = Packetizer(self.logger.logQ) # packetizer must be created here, because it's parameters will change based on the transport!
-		transport = HTTP11Transport(reader, writer)
-		await transport.run()
-		agent = MultiplexorAgent(self.logger.logQ, transport, packetizer)
-		await agent.run()
-		await self.agent_dispatch_queue.put((agent, 'CONNECTED'))
-		await agent.wait_closed()
-		await self.logger.info('Agent disconnected! %s:%s' % (remote_ip, remote_port))
-		await self.agent_dispatch_queue.put((agent, 'DISCONNECTED'))
+		await self.process_request(reader, writer)
 		
 	@mpexception
 	async def run(self):
