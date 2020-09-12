14e7b2768a965a55aa154ef0351ebdf4f8a7fabf
skelsec <info@skelsec.com>
2020-09-12 13:35:49 -0700

    removing test file

--- pt.py
+++
@@ -1,7 +0,0 @@
-
-
-import requests 
-URL = "http://127.0.0.1:8765/alma"
-PARAMS = {'data':'asdfq4t134to9013jg[oqaike;vmaq;eo54itj1	34oin;'} 
-r = requests.post(url = URL, data = PARAMS)
-print(r.text)
